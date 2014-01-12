9f81a0e26661ee4f9ac66331c9f11f6b66b45a5b
Gary C. Davison <gary.davison@jhu.edu>
2014-01-11 23:38:17 -0500

    Section 6.3 Done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,17 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
@@ -47,9 +51,9 @@ describe User do
         expect(@user).to be_valid
       end
     end
-  end
+  end #valid email
 
-describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -57,8 +61,27 @@ describe "when email address is already taken" do
     end
 
     it { should_not be_valid }
-  end
+  end #emailtaken
 
+  it { should respond_to(:authenticate) }
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end #tooshort
 
-end
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end #valid password
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end  #invalid password
+  end #return value of authenticate method
+end #describe user
+
 
