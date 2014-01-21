9abb3a9716d242f55a28a6523e504620879b747e
Gary C. Davison <gary.davison@jhu.edu>
2014-01-21 01:53:54 -0500

    Section 8.2.6 Done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,5 +19,12 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def sign_out
+    current_user.update_attribute(:remember_token,
+                                  User.encrypt(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end  #sign_out
+
 
 end
