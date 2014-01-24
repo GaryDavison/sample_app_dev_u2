3de723bdcb9d6668684a8feecee6f52547cfd52d
Gary C. Davison <gary.davison@jhu.edu>
2014-01-24 00:02:05 -0500

    Section 9.1.3 done.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -100,7 +100,10 @@ describe "User pages" do
   #edit page
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
+    before do
+      sign_in user
+      visit edit_user_path(user) 
+    end # before
 
     describe "page" do
       it { should have_content("Update your profile") }
@@ -113,6 +116,27 @@ describe "User pages" do
 
       it { should have_content("error") }
     end  #edit page - invalid information
+
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end   #valid before
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+
+    end #valid info
+
   end #edit page
 
 end # user pages
