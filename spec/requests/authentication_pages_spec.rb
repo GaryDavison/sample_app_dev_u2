fa3c831204701cf3efcb408d01d21945b7c6843c
Gary C. Davison <gary.davison@jhu.edu>
2014-01-27 22:11:50 -0500

    Exercise 9.6.4 Done

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -111,9 +111,10 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
+#          fill_in "Email",    with: user.email
+#          fill_in "Password", with: user.password
+#          click_button "Sign in"
         end  #before
 
         describe "after signing in" do
